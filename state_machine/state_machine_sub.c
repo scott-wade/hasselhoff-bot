c284434695e010b4060dcca83ea25e6e5fb78e0c
scott-wade <78106639+scott-wade@users.noreply.github.com>
2024-11-13 18:43:48 +0000

    Added stubs for state machine (#1)

---
+++ state_machine/state_machine_sub.c
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  * @file    state_machine_sub.c 
+  * @author  scottwad@andrew.cmu.edu
+  * @version 1.0
+  * @date    November -2024
+  * @brief   State Machine for Hasselhoff Submarine
+  ******************************************************************************
+  */
+
+
+#include "state_machine_sub.h"
+
+void init_sub(void){
+    /* Initialize sub state machine */ 
+
+
+}
+
+
+void event_handler_sub(void){
+    /* Checks and handles events for sub */
+
+
+}
---
+++ state_machine/state_machine_sub.h
@@ -0,0 +1,26 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STATE_MACHINE_SUB_H_
+#define __STATE_MACHINE_SUB_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+
+/* MACROS for everyone--------------------------------------------------------*/
+
+
+
+/*Function definitions---------------------------------------------------------*/
+void init_sub(void);
+void event_handler_sub(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__STATE_MACHINE_SUB_H */
