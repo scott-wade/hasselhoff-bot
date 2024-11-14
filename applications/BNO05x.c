32a1338e7f885dadef6545dd92fd084e7368ec49
Sidney Nimako <snibo13@gmail.com>
2024-11-13 23:46:08 -0500

    BNO Bringup

---
+++ applications/BNO05x.c
@@ -0,0 +1,20 @@
+void init_IMU()
+{
+    // Confirm that SPI is initialised
+    // If not initialised, initialise
+    // Set RST Pin mode (if applicable)
+    // Set RST High (if applicable)
+    // Set CS Pin mode 
+    // Set CS High
+
+}
+
+void hardware_reset()
+{
+    // RST High
+    // Delay 10 ms
+    // RST Low
+    // Delay 10 ms
+    // RST High
+    // Delay 10 ms
+}
