c2190b9d611c86c5581006f4e16b309b25e27dfb
scott-wade <stwade@bu.edu>
2024-11-15 01:13:01 +0000

    made tests folder

---
+++ tests/test_spi.h
@@ -0,0 +1,15 @@
+#ifndef __TEST_SPI_H_
+#define __TEST_SPI_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void testReadWriteRegOpMode(void);
+void testReadRegOpMode(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__TEST_SPI_H_ */
